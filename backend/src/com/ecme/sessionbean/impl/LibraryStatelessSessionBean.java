49d23c7c055c6f78938443c2f514fc0aae04600b
edgar-neology <emercado@neology-rfid.com>
2016-07-28 18:24:38 -0500

    Stateless lifecycle

--- backend/src/com/ecme/sessionbean/impl/LibraryStatelessSessionBean.java
+++ backend/src/com/ecme/sessionbean/impl/LibraryStatelessSessionBean.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.AfterBegin;
+import javax.ejb.AfterCompletion;
+import javax.ejb.BeforeCompletion;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
+import javax.ejb.Remove;
 import javax.ejb.Stateless;
 
 import com.ecme.sessionbean.ILibraryStatelessSessionBeanRemote;
@@ -19,15 +26,18 @@ public class LibraryStatelessSessionBean implements ILibraryStatelessSessionBean
 	 
 	   public void addBook(String bookName) {
 	      bookShelf.add(bookName);
-	      System.out.println("Agregando libro stateles:"+bookName);
+	      System.out.println("!!!!!Agregando libro stateless:"+bookName);
 	   }    
 	 
 	   public List<String> getBooks() {
 	      return bookShelf;
 	   }
-	   @PostConstruct
-	   public void init(){
-		   System.out.println("Creando STATELESS BEAN:");
-	   }
-
+		@PostConstruct
+		public void init(){
+			System.out.println("_______________@PostConstruct EXECUTED ___________________"+Thread.currentThread().getName());
+		}
+		@PreDestroy
+		public void destroy(){
+			System.out.println("_______________@PreDestroy EXECUTED ___________________"+Thread.currentThread().getName());
+		}
 }
